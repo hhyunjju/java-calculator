977a5f130ba3bce7738d0b9d2150969c8094c2ae
minseok <tir2986@naver.com>
2024-02-29 09:08:38 +0900

    refactor: change structure

--- src/Application.java
+++ src/Application.java
@@ -6,19 +6,36 @@ public class Application {
     public static void main(String[] args) throws IOException {
         BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
 
-        while (true) {
-            System.out.print("수식을 입력해주세요(종료: x) : ");
+        Calculator calculator = new Calculator();
 
-            // 수식 입력
-            // 빈 입력 혹은 x 입력 시 종료
+        //TODO 만든 연산자 등록
+        //calculator.registerOperator(....);
+
+        String operatorOptions = calculator.describeAllOperators();
+
+        while (true) {
+            System.out.println(operatorOptions);
+            System.out.println("종료: x");
+
             String input = consoleReader.readLine();
+
             if (input.isBlank() || input.charAt(0) == 'x') {
                 break;
             }
 
-            // 결과 출력
-            String result = Calculator.calculateExpression(input);
-            System.out.println(result);
+            int opNum = Integer.parseInt(input);
+            char operatorChar = calculator.getOperatorChar(opNum);
+
+            System.out.print("num1: ");
+            double n1 = Double.parseDouble(consoleReader.readLine());
+            System.out.print("num2: ");
+            double n2 = Double.parseDouble(consoleReader.readLine());
+
+            double result = calculator.calculate(n1, n2, opNum);
+
+            System.out.printf("%f %c %f = %f\n",
+                    n1, operatorChar, n2, result
+            );
         }
 
         consoleReader.close();
--- src/Calculator.java
+++ src/Calculator.java
@@ -1,49 +1,60 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculator {
 
-    /**
-     * 수식을 입력받아 계산 결과를 정해진 포맷의 문자열로 만들어 준다.
-     *
-    * @param exp 수식
-     * @return 수식 계산 결과 "{수식} = {결과}"
-    * */
-    public static String calculateExpression(String exp) {
+    public interface Operator {
 
-        // 수식에서 피연산자와 연산자 추출
-        double[] operands = extractOperands(exp);
-        char operator = extractOperator(exp);
+        /**
+         * 연산 수행 메소드
+         * */
+        double operate(double n1, double n2);
 
-        // 계산
-        double result = calculate(operands[0], operands[1], operator);
+        /**
+         * 연산자 char 리턴
+         * */
+        char getOperatorChar();
 
-        // 출력 형식에 맞춰서 문자열 리턴
-        return formatResult(exp, result);
+        /**
+         * 어떤 연산인지 설명
+         * */
+        String describe();
     }
 
-    private static double[] extractOperands(String exp) {
-        // TODO 1
-        // 입력 받은 수식에서 피연산자 2개를 추출한다.
-        return new double[2];
+    private final List<Operator> operators = new ArrayList<>();
+
+    public double calculate(double n1, double n2, int opNum) {
+        if (opNum < 0 || opNum >= operators.size()) {
+            throw new IllegalArgumentException("맞는 연산자가 없습니다.");
+        }
+
+        Operator operator = operators.get(opNum);
+
+        return operator.operate(n1, n2);
     }
 
-    private static char extractOperator(String exp) {
-        // TODO 2
-        // 입력 받은 수식에서 연산자를 추출한다.
-        return ' ';
+    public void registerOperator(Operator operator) {
+        operators.add(operator);
     }
 
-    private static String formatResult(String exp, double result) {
-        // TODO 3
-        // "{exp} = {result}" 형태로 문자열 만들어서 리턴한다.
-        return "";
+    public char getOperatorChar(int opNum) {
+        if (opNum < 0 || opNum >= operators.size()) {
+            throw new IllegalArgumentException("맞는 연산자가 없습니다.");
+        }
+
+        return operators.get(opNum).getOperatorChar();
     }
 
-    private static double calculate(double n1, double n2, char operator) {
-        return switch (operator) {
-            case '+' -> n1 + n2;
-            case '-' -> n1 - n2;
-            case '*' -> n1 * n2;
-            case '/' -> n1 / n2;
-            default -> throw new IllegalArgumentException("잘못된 연산자 입니다.");
-        };
+    public String describeAllOperators() {
+        StringBuilder descriptions = new StringBuilder();
+
+        for (int i=0; i<operators.size(); i++) {
+            Operator operator = operators.get(i);
+            descriptions.append(String.format(
+                    "%d: %s\n", i, operator.describe()
+            ));
+        }
+
+        return descriptions.toString();
     }
 }
